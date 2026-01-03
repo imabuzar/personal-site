fb201c12dbc78b116c39a8688f3892f589413c39
Abuzar <theopenorbit@gmail.com>
2026-01-03 23:51:33 +0500

    Create site structure and add header, footer, theme switcher, and 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [/** @type {any} */ (tailwindcss())],
+  },
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'catppuccin-latte',
+        dark: 'vesper',
+      },
+    },
+  },
+});
